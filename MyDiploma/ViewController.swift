8f3e636d83497bac17ca4ece7f994be31814596c
loringit <yakupov.bulat@gmail.com>
2017-05-09 16:32:42 +0300

    initial

---
+++ MyDiploma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyDiploma
+//
+//  Created by Булат Якупов on 09.05.17.
+//  Copyright © 2017 Булат Якупов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
